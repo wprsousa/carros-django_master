92d22f77dd5b760288296c6f60242279295cb37d
Wellington Pedro <wprsousa@gmail.com>
2024-01-10 06:59:44 -0300

    includes login_required in view

--- cars/views.py
+++ cars/views.py
@@ -1,31 +1,16 @@
-# from django.shortcuts import render, redirect
-# from django.views import View
-from django.views.generic import ListView, CreateView
+from django.contrib.auth.decorators import login_required
+from django.urls import reverse_lazy
+from django.utils.decorators import method_decorator
+from django.views.generic import (
+    ListView,
+    CreateView,
+    DetailView,
+    UpdateView,
+    DeleteView,
+)
 
 from cars.models import Car
 from cars.forms import CarModelForm
-
-
-# Feito com Function Based View
-# def cars_view(request):
-#     cars = Car.objects.all().order_by("model")
-#     search = request.GET.get("search")
-#
-#     if search:
-#         cars = Car.objects.filter(model__icontains=search).order_by("model")
-#
-#     return render(request, "cars.html", {"cars": cars})
-
-# Feito com Classed Based View
-# class CarsView(View):
-#     def get(self, request):
-#         cars = Car.objects.all().order_by("model")
-#         search = request.GET.get("search")
-#
-#         if search:
-#             cars = Car.objects.filter(model__icontains=search).order_by("model")
-#
-#         return render(request, "cars.html", {"cars": cars})
 
 
 class CarsListView(ListView):
@@ -41,33 +26,31 @@ class CarsListView(ListView):
         return cars
 
 
-# Feito com Function Based View
-# def new_car_view(request):
-#     if request.method == "POST":
-#         new_car_form = CarModelForm(request.POST, request.FILES)
-#         if new_car_form.is_valid():
-#             new_car_form.save()
-#             return redirect("cars_list")
-#     else:
-#         new_car_form = CarModelForm()
-#     return render(request, "new_car.html", {"new_car_form": new_car_form})
-
-# Feito com Function Based View
-# class NewCarView(View):
-#     def get(self, request):
-#         new_car_form = CarModelForm()
-#         return render(request, "new_car.html", {"new_car_form": new_car_form})
-#
-#     def post(self, request):
-#         new_car_form = CarModelForm(request.POST, request.FILES)
-#         if new_car_form.is_valid():
-#             new_car_form.save()
-#             return redirect("cars_list")
-#         return render(request, "new_car.html", {"new_car_form": new_car_form})
+class CarDetailView(DetailView):
+    model = Car
+    template_name = "car_detail.html"
 
 
+@method_decorator(login_required(login_url="login"), name="dispatch")
 class NewCarCreateView(CreateView):
     model = Car
     form_class = CarModelForm
     template_name = "new_car.html"
+    success_url = "/cars/"
+
+
+@method_decorator(login_required(login_url="login"), name="dispatch")
+class CarUpdateView(UpdateView):
+    model = Car
+    form_class = CarModelForm
+    template_name = "car_update.html"
+
+    def get_success_url(self):
+        return reverse_lazy("car_detail", kwargs={"pk": self.object.pk})
+
+
+@method_decorator(login_required(login_url="login"), name="dispatch")
+class CarDeleteView(DeleteView):
+    model = Car
+    template_name = "car_delete.html"
     success_url = "/cars/"
