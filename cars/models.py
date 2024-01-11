fe63c4917e04e9c57ff8405969a2a025d487fea8
Wellington Pedro <wprsousa@gmail.com>
2024-01-11 06:56:55 -0300

    included signals.py

--- cars/models.py
+++ cars/models.py
@@ -12,13 +12,25 @@ class Brand(models.Model):
 class Car(models.Model):
     id = models.AutoField(primary_key=True)
     model = models.CharField(max_length=200)
-    brand = models.ForeignKey(Brand, on_delete=models.PROTECT,
-                              related_name='car_brand')
+    brand = models.ForeignKey(Brand, on_delete=models.PROTECT, related_name="car_brand")
     factory_year = models.IntegerField(blank=True, null=True)
     model_year = models.IntegerField(blank=True, null=True)
     plate = models.CharField(max_length=10, blank=True, null=True)
     value = models.FloatField(blank=True, null=True)
-    photo = models.ImageField(upload_to='cars/', blank=True, null=True)
+    photo = models.ImageField(upload_to="cars/", blank=True, null=True)
 
     def __str__(self):
         return self.model
+
+
+class CarInventory(models.Model):
+    cars_count = models.IntegerField()
+    cars_value = models.FloatField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["-created_at"]
+
+    def __str__(self):
+        return f"{self.cars_count} - {self.cars_value}"
+
